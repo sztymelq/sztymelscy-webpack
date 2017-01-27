1308b3fb813dc31e72752c2ec5d3a49cb4aaa132
Sztymelski <sztymelskip@wrosztymelskip.local>
2017-01-27 17:26:33 +0100

    hero goes to components, flipClock sticks to images, flipclock opacity

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,12 +1,11 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
+
 import User from './user/user';
 import Jumbotron from './jumbotron/jumbotron';
 
 let commonModule = angular.module('app.common', [
   Navbar,
-  Hero,
   User,
   Jumbotron
 ])
--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
+import Hero from './hero/hero';
 
 let componentModule = angular.module('app.components', [
-  Home,
-  About
+    Home,
+    About,
+    Hero
 ])
-
-.name;
+    .name;
 
 export default componentModule;
