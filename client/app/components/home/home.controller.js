adff5830dc35999d08008b27d6cabe5199e2a5f5
Sztymelski <sztymelskip@wrosztymelskip.local>
2017-01-30 11:51:50 +0100

    expose loader installed, home.ctrl.js  scrollMagic

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,6 +1,19 @@
+import ScrollMagic from 'ScrollMagic';
+
 class HomeController {
   constructor() {
     this.name = 'home';
+  }
+
+  $onInit() {
+      const smCtrl = new ScrollMagic.Controller();
+
+      new ScrollMagic.Scene({triggerElement: "#trigger"})
+          .setVelocity(".section.jumbotron", {opacity: 0}, {duration: 400})
+          .addTo(smCtrl);
+
+    console.log('on init!');
+    console.log('ScrollMagic', ScrollMagic);
   }
 }
 
