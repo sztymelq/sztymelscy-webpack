cf327fb002a0e26b2c88f80e10d3b89e9c1d4dc9
Piotr Sztymelski <sztymel@gmail.com>
2017-02-04 16:18:30 +0100

    compressed images, altered colors, scrollMagic applied to all jumbotrones

--- client/app/components/hero/hero.controller.js
+++ client/app/components/hero/hero.controller.js
@@ -1,28 +1,28 @@
 class HeroController {
     constructor($interval) {
         const SLIDES_INTERVAL = 4000;
-        const MS_PER_DAY = 1000;
-        const weddingDate = new Date(2017, 7, 5, 16, 0, 0, 0);
         this.maxImages = 5;
         this.currentImage = 1;
         this.images = [...Array(this.maxImages).keys()];
 
-        this.$onInit = () => {
-            new FlipClock($('.countdown-clock'), (weddingDate - new Date()) / MS_PER_DAY, {
-                countdown: true,
-                clockFace: 'DailyCounter'
-            });
-
-            const labels = $('.flip-clock-label');
-            labels[0].innerHTML = 'Dni';
-            labels[1].innerHTML = 'Godziny';
-            labels[2].innerHTML = 'Minuty';
-            labels[3].innerHTML = 'Sekundy';
-        };
-
         $interval(() => {
             this.currentImage = (this.currentImage + 1) % this.maxImages;
         }, SLIDES_INTERVAL);
+    }
+
+    $onInit = () => {
+        const weddingDate = new Date(2017, 7, 5, 16, 0, 0, 0);
+        const MS_PER_DAY = 1000;
+        new FlipClock($('.countdown-clock'), (weddingDate - new Date()) / MS_PER_DAY, {
+            countdown: true,
+            clockFace: 'DailyCounter'
+        });
+
+        const labels = $('.flip-clock-label');
+        labels[0].innerHTML = 'Dni';
+        labels[1].innerHTML = 'Godziny';
+        labels[2].innerHTML = 'Minuty';
+        labels[3].innerHTML = 'Sekundy';
     }
 
     computeImagePath(name) {
--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,21 +1,27 @@
 class HomeController {
   constructor() {
     this.name = 'home';
+    this.scrollMagicController = new ScrollMagic.Controller();
   }
 
   $onInit() {
-      const smCtrl = new ScrollMagic.Controller();
-      new ScrollMagic.Scene({triggerElement: "#trigger"})
-          .setVelocity("#onas > .jumbotron", {opacity: 0}, {duration: 400})
-          .addTo(smCtrl);
+      this.registerVelocity('onas');
+      this.registerVelocity('slub');
+      this.registerVelocity('wesele');
+      this.registerVelocity('dojazd');
+      this.registerVelocity('zakwaterowanie');
 
       // new ScrollMagic.Scene({
       //     triggerElement: "#trigger"
       // }).setTween("#onas", 0.5, {backgroundColor: "green", scale: 2.5})
       //     .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
       //     .addTo(smCtrl);
+  }
 
-    console.log('ScrollMagic', ScrollMagic);
+  registerVelocity(elementId) {
+    new ScrollMagic.Scene({triggerElement: `#${elementId}-trigger`})
+          .setVelocity(`#${elementId} > .jumbotron`, {opacity: 1}, {duration: 400})
+          .addTo(this.scrollMagicController);
   }
 }
 
