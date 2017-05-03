306395ccfd686143a320adfbf9c1018150ae5f71
Piotr Sztymelski <sztymelskip@ryanair.com>
2017-05-03 14:05:12 +0200

    new logo animation on keyframes

--- client/app/components/hero/hero.controller.js
+++ client/app/components/hero/hero.controller.js
@@ -1,18 +1,5 @@
 class HeroController {
-  constructor($timeout) {
-    this.$timeout = $timeout;
-  }
 
-  $onInit() {
-    this.$timeout(() => {
-      this.logoNamesVisible = true;
-    }, 1000);
-
-    this.$timeout(() => {
-      this.logoWelcomeVisible = true;
-    }, 2500);
-  }
 }
 
-HeroController.$inject = ['$timeout'];
 export default HeroController;
