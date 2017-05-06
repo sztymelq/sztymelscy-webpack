5481cea6481482ce773cdd12aa8f3ed7d1c66f33
Piotr Sztymelski <sztymel@gmail.com>
2017-05-06 15:23:49 +0200

    about component removed,, new font introduced, package.json altered

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Hero from './hero/hero';
 import WeddingDate from './weddingDate/weddingDate';
 
 export default angular.module('app.components', [
   Home,
-  About,
   Hero,
   WeddingDate
 ]).name;
